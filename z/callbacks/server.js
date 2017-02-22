aa208b0ee42cfa26da167bc49e8d199f5ea29141
Gabriel Page <gabriel.page@gamesparks.com>
2017-02-22 11:04:30 +0000

    no message

--- z/callbacks/server.js
+++ z/callbacks/server.js
@@ -2,8 +2,10 @@
 //
 // Cloud Code for server, write your code here to customise the GameSparks platform.
 //
-// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm
 //
 // ====================================================================================================
 
 Spark.setScriptData("name", 'value')
+
+//test branch
